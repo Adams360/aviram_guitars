cf36ec3afd4dd37446f6a58bd9fa2498b19b7b07
Adam Shaul <adamshaul360@gmail.com>
2020-01-09 00:16:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('header');
+
+
+hamburger.addEventListener('click', () => {
+    header.classList.toggle('reveal');
+})
